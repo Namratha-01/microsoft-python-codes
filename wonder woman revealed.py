6b0fbdf544035555117137e16ff0bb7ba902667d
Namratha <namrathaj2001@gmail.com>
2020-10-22 23:21:00 +0530

    decoding the secret code

---
+++ wonder woman revealed.py
@@ -0,0 +1,18 @@
+'''
+# Associate the variable diana with the value "WONDER WOMAN 1984"
+diana = "WONDER WOMAN 1984"
+
+# Print a message with the true identity of Diana
+print( "I believe Diana is actually " + diana )
+'''
+# Define a power (function) to chant a phrase
+def chant( phrase ):
+    # Glue three copies together and print it as a message
+    print( phrase + phrase + phrase )
+
+
+# Invoke the power chant on the phrase "WONDER WOMAN 1984!"
+chant( "WONDER WOMAN 1984!" )
+def lassoLetter( letter, shiftAmount ):
+    lassoLetter('a', 2)
+   
