eeb7172301f71c299a1b1145199564b688df4f91
Namratha <namrathaj2001@gmail.com>
2020-10-22 23:26:29 +0530

    how to call a function

--- wonder woman revealed.py
+++ wonder woman revealed.py
@@ -5,7 +5,7 @@ diana = "WONDER WOMAN 1984"
 # Print a message with the true identity of Diana
 print( "I believe Diana is actually " + diana )
 '''
-# Define a power (function) to chant a phrase
+# Define a power (function) to chant a phrase.
 def chant( phrase ):
     # Glue three copies together and print it as a message
     print( phrase + phrase + phrase )
